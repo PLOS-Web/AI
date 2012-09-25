29922d90b974091f855541a8df3d1695a86ccaf0
Jack LaBarba <brakit@gmail.com>
2012-09-25 16:17:09 -0700

    Adds relative path template magic

--- ai/settings.py
+++ ai/settings.py
@@ -1,5 +1,10 @@
 # Django settings for ai project.
 
+import os.path
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+PACKAGE_ROOT = os.path.abspath(os.path.dirname(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -107,6 +112,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PACKAGE_ROOT, "templates"),
 )
 
 INSTALLED_APPS = (
