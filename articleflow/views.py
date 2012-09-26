f8b3df6d84847567d28cf4c66bd395bf4b571ca5
Jack LaBarba <brakit@gmail.com>
2012-09-26 15:28:56 -0700

    Adds 404 exception

--- articleflow/views.py
+++ articleflow/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response
 from articleflow.models import Article, ArticleState, State
 
@@ -8,6 +8,10 @@ def grid(request):
     return render_to_response('articleflow/grid.html', ctx)
 
 def detail(request, doi=None):
-    article = Article.objects.get(doi=doi)
+    try:
+        article = Article.objects.get(doi=doi)
+    except Article.DoesNotExist:
+        raise Http404
+        
     ctx = {'article': article}
     return render_to_response('articleflow/detail.html',ctx)
