f64244716e62d5f12c46a886003353efd2bd2c43
Jack LaBarba <brakit@gmail.com>
2012-10-08 11:25:52 -0700

    Adds twitter bootstrap to project

--- ai/settings.py
+++ ai/settings.py
@@ -61,7 +61,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PACKAGE_ROOT, "site_media", "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -69,9 +69,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PACKAGE_ROOT, "static"),
 )
 
 # List of finder classes that know how to find static files in
@@ -128,6 +126,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     # Project Apps
     'debug_toolbar',
+    'bootstrap',
     'articleflow',
 )
 
