81ef3fa39276c207dd256d311f2531be3b3c566d
Jack LaBarba <brakit@gmail.com>
2012-12-14 15:50:23 -0800

    Fixes context problems with error detail view

--- errors/views.py
+++ errors/views.py
@@ -1,11 +1,21 @@
 from django.views.generic import ListView
 
 from errors.models import ErrorSet, Error
+from articleflow.models import Article
 
 class Errors(ListView):
     template_name = 'errors/error_list.html'
 
     def get_queryset(self):
         return ErrorSet.objects.filter(article__doi=self.kwargs['doi']).select_related('Errors')
+    
+    def get_context_data(self, **kwargs):
+        article = Article.objects.get(doi=self.kwargs['doi'])
+        context = ({
+                'article': article,
+                'errorset_list': self.get_queryset()
+            })
+        print context
+        return context
 
     
