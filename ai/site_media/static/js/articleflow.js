30e43283082a89716e22427a9327cf8219bf3637
dweebit <dweebit@gmail.com>
2013-01-25 16:17:00 -0800

    Collects some static

--- ai/site_media/static/js/articleflow.js
+++ ai/site_media/static/js/articleflow.js
@@ -10,13 +10,27 @@ function switch_article_state_ajax(article_pk, requested_transition_pk, error_ms
 	datatype: "application/json; charset=utf-8",
 	data: postData
     }).done(function (returnedData){
-	console.log(returnedData);
-	if returnedData.error{
-	    error_msg_obj.html(returnedData.error);
-	} else if returnedData.open_item_error{
-	    error_msg_obj.html("Can't advance state, theres still " + returnedData.open_item_error.open_issues + " open issues and " + returnedData.open_item_error.open_errors + " open errors." );
+	console.log(returnedData.error);
+	
+	if (returnedData.error) {
+	    alert(returnedData['error']);
+	} 
+	
+	else if (returnedData.open_item_error){
+	    alert("Can't do that while there are open items that need action:\n\t" + returnedData.open_item_error.open_issues + " open issues\n\t" + returnedData.open_item_error.open_errors + " open errors." );
 	} else {
 	    document.location.href = returnedData.redirect_url
 	}
+	
     });
 }
+
+$(document).ready(function(){
+    $('#article-note-form').on('hide', function(){
+	$('#article-note-collapse-toggle').html('[+]');
+    });
+    
+    $('#article-note-form').on('show', function(){
+	$('#article-note-collapse-toggle').html('[-]');
+    });
+});
--- ai/site_media/static/js/grid.js
+++ ai/site_media/static/js/grid.js
@@ -1,1 +1,11 @@
-$('#datepicker').datepicker();
+$(document).ready(function() {
+    $('.datepicker').datepicker();
+
+    $('#search-panel').on('hide', function() {;
+	$('#search-collapse-toggle').html('[+] search');
+    });
+    
+    $('#search-panel').on('show', function() {
+	$('#search-collapse-toggle').html('[-] search');
+    });
+});
