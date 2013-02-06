43fa156f5673b26725d193617fddc5c35006cbbe
Jack LaBarba <brakit@gmail.com>
2013-02-05 17:41:13 -0800

    Let's try that again

--- dumpdata.sh
+++ dumpdata.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-python manage.py dumpdata --natural articleflow.article articleflow.journal articleflow.state articleflow.transition issues.category > initial_data.json
+python manage.py dumpdata --natural articleflow.journal articleflow.state articleflow.transition issues.category > initial_data.json
