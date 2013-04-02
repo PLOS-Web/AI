c216df03421f9393ea3d90a29a3a5975e2d84a3b
Jack LaBarba <brakit@gmail.com>
2013-04-02 09:20:44 -0700

    Migration to add bookkeeping fields to articleflow

--- articleflow/models.py
+++ articleflow/models.py
@@ -26,6 +26,9 @@ class State(models.Model):
     auto_assign = models.IntegerField(default=1, choices=AUTO_ASSIGN)
     progress_index = models.IntegerField(default=0)
 
+    #Bookkeeping 
+    created = models.DateTimeField(default=datetime.datetime.utcnow().replace(tzinfo=utc))
+    last_modified = models.DateTimeField(auto_now=True)
     def __unicode__(self):
         return self.name
 
@@ -47,6 +50,7 @@ class ArticleState(models.Model):
     from_transition = models.ForeignKey('Transition', related_name='articlestates_created' , null=True, blank=True, default=None)
     from_transition_user = models.ForeignKey(User, related_name='articlestates_created',null=True, blank=True, default=None)
 
+    #Bookkeeping 
     created = models.DateTimeField(default=datetime.datetime.utcnow().replace(tzinfo=utc))
     last_modified = models.DateTimeField(auto_now=True)
 
@@ -92,6 +96,7 @@ class Journal(models.Model):
     em_url_prefix = models.CharField(max_length=200) #the journal name modifier for EM's url scheme
     em_ambra_stage_prefix = models.CharField(max_length=200) #the journal name modifier for stage Ambra's URL scheme
     
+    #Bookkeeping
     created = models.DateTimeField(default=datetime.datetime.utcnow().replace(tzinfo=utc))
     last_modified = models.DateTimeField(auto_now=True)    
 
@@ -114,7 +119,7 @@ class Article(models.Model):
     em_ms_number = models.CharField(max_length=50, null=True, blank=True, default=None)
     em_max_revision = models.IntegerField(null=True, blank=True, default=None)
     
-
+    #Bookkeeping
     created = models.DateTimeField(default=datetime.datetime.utcnow().replace(tzinfo=utc))
     last_modified = models.DateTimeField(auto_now=True)
 
@@ -217,6 +222,9 @@ class Transition(models.Model):
     disallow_open_items = models.BooleanField(default=False)
     allowed_groups = models.ManyToManyField(Group, related_name="allowed_transitions")
     preference_weight = models.IntegerField()
+
+    #Bookkeeping 
+    created = models.DateTimeField(default=datetime.datetime.utcnow().replace(tzinfo=utc))
     last_modified = models.DateTimeField(auto_now=True)
     
     def __unicode__(self):
@@ -240,7 +248,8 @@ class Transition(models.Model):
 class AssignmentHistory(models.Model):
     user = models.ForeignKey(User, related_name='assignment_histories')
     article_state = models.ForeignKey('ArticleState', related_name='assignment_histories')
-    
+
+    #Bookkeeping
     created = models.DateTimeField(default=datetime.datetime.utcnow().replace(tzinfo=utc))
     last_modified = models.DateTimeField(auto_now=True)
 
@@ -252,7 +261,7 @@ class AssignmentRatio(models.Model):
     state = models.ForeignKey('State', related_name='assignment_weights')
     weight = models.IntegerField(null=True, blank=True, default=None)
 
-    # bookkeeping
+    #Bookkeeping
     created = models.DateTimeField(default=datetime.datetime.utcnow().replace(tzinfo=utc))
     last_modified = models.DateTimeField(auto_now=True)
 
