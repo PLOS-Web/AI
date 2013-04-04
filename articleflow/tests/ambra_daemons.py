98c7012defbd56dafbcc35eabded446c6098cd91
dweebit <dweebit@gmail.com>
2013-04-04 12:24:51 -0700

    Adds test for assign_ingested

--- articleflow/tests/ambra_daemons.py
+++ articleflow/tests/ambra_daemons.py
@@ -1,29 +1,45 @@
 from django.test import TestCase
 from articleflow.daemons.transition_tasks import *
 from articleflow.models import *
-from ambra_query import *
+from articleflow.daemons.ambra_query import *
 
 class TransitionTasksTestCase(TestCase):
     '''
     art 1:  pone.0058985
             pulled_article should be 'Pulled' in AI, but
             ingested or pubbed on stage
+    art 2:  pone.0059284
+            Same
+    art 3:  pone.0060568
+            Same
+            
     
     '''
     
-    fixtures = ['transitions_testdata.json']
+    fixtures = ['initial_data.json', 'transitions_testdata.json']
 
-    def setUp():
+    def setUp(self):
         self.stage_c = AmbraStageConnection()
-        self.live_c = AmbraProdConnection()
+        #self.live_c = AmbraProdConnection()
 
-    def tearDown():
+    def tearDown(self):
         del self.stage_c
-        del self.live_c
+        #del self.live_c
 
-    def test_assign_ingested_doi(self):
+    def test_assign_ingested_article(self):
         # art 1
-        pulled_article = Article.object.get(doi='pone.0058985')
-        assign_ingested_doi(pulled_article, self.stage_c)
-        ingested_state = State.objects.get(name='ingested')
-        assertEqual(pulled_article.current_state, ingested_state)
+        pulled_article = Article.objects.get(doi='pone.0058985')
+        assign_ingested_article(pulled_article, self.stage_c)
+        ingested_state = State.objects.get(name='Ingested')
+        self.assertEqual(pulled_article.current_state, ingested_state)
+
+    def test_assign_ingested(self):
+        assign_ingested(self.stage_c)
+        ingested_state = State.objects.get(name='Ingested')
+        # art 2
+        art_2 = Article.objects.get(doi='pone.0059284')
+        # art 3
+        art_3 = Article.objects.get(doi='pone.0060568')
+        self.assertEqual(art_2.current_state, ingested_state)
+        self.assertEqual(art_3.current_state, ingested_state)
+        
