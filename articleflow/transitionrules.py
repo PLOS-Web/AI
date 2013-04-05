9c37a4712fb046539ddab92d2f08d3a1887ef8b2
Jack LaBarba <brakit@gmail.com>
2013-04-05 12:43:38 -0700

    Keeps open errors from blocking publishing

--- articleflow/transitionrules.py
+++ articleflow/transitionrules.py
@@ -1,6 +1,6 @@
 from articleflow.models import Article
 from issues.models import Issue, IssueStatus
-from errors.models import Error, ErrorStatus
+from errors.models import Error, ErrorStatus, ErrorSet
 
 # ghetto polymorphic function
 def ready_to_ignore(item):
@@ -22,11 +22,14 @@ def article_count_open_items(article):
     for i in article.issues.all():
         if not ready_to_ignore(i):
             open_issues += 1
-            
-    latest_errorset = article.error_sets.latest('created')
-    for e in latest_errorset.errors.all():
-        if not ready_to_ignore(i):
-            open_errors += 1
+
+    try:
+        latest_errorset = article.error_sets.latest('created')
+        for e in latest_errorset.errors.all():
+            if not ready_to_ignore(i):
+                open_errors += 1
+    except ErrorSet.DoesNotExist, e:
+        open_errors = 0
 
     return {
         'open_issues': open_issues,
@@ -35,6 +38,6 @@ def article_count_open_items(article):
 
 def article_no_open_items(article):
     items = article_count_open_items(article)
-    if (items.open_issues == 0 and items.open_errors == 0):
+    if (items.open_issues == 0):
         return True
     return False
