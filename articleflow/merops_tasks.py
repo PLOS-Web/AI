18b2f03effc2c064c1d6d42508b0dfe8bb7dd589
Jack LaBarba <brakit@gmail.com>
2013-05-30 08:56:09 -0700

    Adds aries delivery watch to merops tasks

--- articleflow/merops_tasks.py
+++ articleflow/merops_tasks.py
@@ -1,24 +1,66 @@
 import os.path
-from articleflow.models import WatchState
+import datetime
+import re
 
+from articleflow.models import Article, ArticleState, State, Typesetter, WatchState
+from ai import settings
 
-def _get_filenames_and_mtime_in_dir(path):
-    return [ (os.path.join(path, f), get_mtime(os.path.join(path, f))) for f in os.listdir(path) if os.path.isfile(os.path.join(path,f))]
+import logging
+logger = logging.getLogger(__name__)
 
+def _get_mtime(file):
+    return datetime.datetime.fromtimestamp(os.stat(file).st_mtime)
 
-# TODO make default here a get or create
-def scan_directory_for_changes(trigger_func, directory, w_s=WatchState(watcher="scan_directory_for_changes")):
-    files = _get_filenames_and_mtime_in_dir(directory)
-    files = filter(w_s.gt_last_mtime,files)
+def _get_filenames_and_mtime_in_dir(path, filename_regex_prog=None):
+    print "PATH %s" % path
+    files = [ (os.path.join(path, f), _get_mtime(os.path.join(path, f))) for f in os.listdir(path) if os.path.isfile(os.path.join(path,f))]
+    if filename_regex_prog:
+        files = filter(lambda x: filename_regex_prog.match(os.path.basename(x[0])), files)
+    return files
+
+def scan_directory_for_changes(ws, trigger_func, directory, filename_regex_prog=None):
+    """make one scan of a directory for all newly modified files.
+    saves last mtime to database using a WatchState object.
+    :param ws: :class:`WatchState` object
+    :param trigger_func: a function to be executed on each new file
+    :param directory: directory path to scan
+    """
+    try:
+        files = _get_filenames_and_mtime_in_dir(directory, filename_regex_prog)
+    except OSError, e:
+        logger.error(e)
+        raise
+    files = filter(lambda x: ws.gt_last_mtime(x[1]),files)
     files = sorted(files, key=lambda f: f[1])
     for f, m_time in files:
-        if m_time > cutoff_cursor:
-            trigger_func(f)
-            if w_s.gt_last_mtime(m_time):
-                w_s.update_last_mtime(m_time)
+        logger.info("Found new file: %s" % f)
+        trigger_func(f)
+        if ws.gt_last_mtime(m_time):
+            ws.update_last_mtime(m_time)
 
-def watch_docs_from_aries(watch_directory):
-    raise NotImplementedError
+def watch_docs_from_aries():
+    def process_doc_from_aries(f):
+        # add article to AI
+        #   extract doi from go.xml
+        si_guid = 'blahblah filler'
+        doi = 'pone.00000001'
+
+        art, new = Article.objects.get_or_create(doi=doi)
+        art.typesetter = Typesetter.objects.get(name='Merops')
+        art.si_guid = si_guid
+
+        art.save()
+        delivery_state = State.objects.get(unique_name='delivered_from_aries')
+        art_s = ArticleState(article=art, state=delivery_state)
+        art_s.save()
+        # extract manuscript, rename to doi.doc
+        # move manuscript to hopper
+                
+    ws, new = WatchState.objects.get_or_create(watcher="merops_aries_delivery")
+    if new:
+        ws.save()
+    zip_prog = re.compile('.*\.zip$')
+    scan_directory_for_changes(ws, process_doc_from_aries, settings.MEROPS_ARIES_DELIVERY, zip_prog)
 
 def queue_doc_meropsing():
     raise NotImplementedError
