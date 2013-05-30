e9c188816b5f38baeb136b2ad29598c7593aa5bb
Jack LaBarba <brakit@gmail.com>
2013-05-30 08:55:22 -0700

    Updates merops dir locations to live dirs

--- ai/merops_settings.py
+++ ai/merops_settings.py
@@ -2,7 +2,8 @@ import os.path
 
 MEROPS_ARIES_DELIVERY = os.path.abspath("/var/spool/delivery/merops/aries-delivery")
 
-MEROPS_MEROPSED_WATCH = os.path.abspath("/var/spool/delivery/merops/merops-watch/")
-MEROPS_MEROPSED_OUTPUT = os.path.abspath("/var/spool/delivery/merops/merops-output/")
-MEROPS_FINISH_XML_WATCH = os.path.abspath("/var/spool/delivery/merops/finishxml-watch/")
-MEROPS_FINISH_XML_OUTPUT = os.path.abspath("/var/spool/delivery/merops/finishxml-output/")
+MEROPS_MEROPSED_WATCH = os.path.abspath("/var/spool/delivery/merops/merops/watch/")
+MEROPS_MEROPSED_OUTPUT = os.path.abspath("/var/spool/delivery/merops/merops/out/")
+MEROPS_FINISH_XML_WATCH = os.path.abspath("/var/spool/delivery/merops/finish/watch/")
+MEROPS_FINISH_XML_OUTPUT = os.path.abspath("/var/spool/delivery/merops/finish/out/"
+)
