013869189310074cfbfaee67abe40cb8c6d936a1
Jack LaBarba <brakit@gmail.com>
2013-06-01 08:31:28 -0700

    Removes SOUTH TEST MIGRATE config from main settings - in local

--- ai/settings.py
+++ ai/settings.py
@@ -14,10 +14,10 @@ from merops_settings import *
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 PACKAGE_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-SOUTH_TESTS_MIGRATE = False
+# SOUTH_TESTS_MIGRATE = False
 
 MANAGERS = ADMINS
 
