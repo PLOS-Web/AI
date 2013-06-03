9b39e3dbb7610258a5f734f8190f641b5982fc1d
Jack LaBarba <brakit@gmail.com>
2013-06-03 14:19:42 -0700

    Fixes merops file naming schema

--- ai/merops_settings.py
+++ ai/merops_settings.py
@@ -18,22 +18,22 @@ MEROPS_FILE_SCHEMA = {
         },
     'meropsed-original.doc': {
         'dir_path': MEROPS_MEROPSED_OUTPUT,
-        'filename_modifier': '-original',
+        'filename_modifier': '-Original',
         'file_extension': 'doc',
         },
-    'meropsed-original.xml': {
+    'meropsed.xml': {
         'dir_path': MEROPS_MEROPSED_OUTPUT,
         'filename_modifier': '',
         'file_extension': 'xml',
         },
     'finishxml.doc': {
         'dir_path': MEROPS_FINISH_XML_OUTPUT,
-        'filename_modifier': '-finishxmlcheck-original',
+        'filename_modifier': '-FinishXMLCheck',
         'file_extension': 'doc',
         },
     'finishxml-original.doc': {
         'dir_path': MEROPS_FINISH_XML_OUTPUT,
-        'filename_modifier': '-finishxmlcheck-original',
+        'filename_modifier': '-FinishXMLCheck-Original',
         'file_extension': 'doc',
         },
     'finishxml.xml': {
