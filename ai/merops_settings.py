46c3801f0fc19a578d9c3195d186f2be72493dc9
Jack LaBarba <brakit@gmail.com>
2013-06-25 09:24:48 -0700

    Adds merops backup locations to merops settings

--- ai/merops_settings.py
+++ ai/merops_settings.py
@@ -3,8 +3,10 @@ import os.path
 MEROPS_ARIES_DELIVERY = os.path.abspath("/var/spool/delivery/merops/aries-delivery")
 
 MEROPS_MEROPSED_WATCH = os.path.abspath("/var/spool/delivery/merops/merops/watch/")
+MEROPS_MEROPSED_WATCH_BU = os.path.abspath("/var/spool/delivery/merops/merops/watch-bu/")
 MEROPS_MEROPSED_OUTPUT = os.path.abspath("/var/spool/delivery/merops/merops/out/")
 MEROPS_FINISH_XML_WATCH = os.path.abspath("/var/spool/delivery/merops/finish/watch/")
+MEROPS_FINISH_XML_WATCH_BU = os.path.abspath("/var/spool/delivery/merops/finish/watch-bu/")
 MEROPS_FINISH_XML_OUTPUT = os.path.abspath("/var/spool/delivery/merops/finish/out/"
 )
 
