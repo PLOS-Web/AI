e15665334e143e71eef811348b51ceae297465de
Jack LaBarba <brakit@gmail.com>
2013-07-25 11:40:06 -0700

    Adds notification context processor

--- ai/settings.py
+++ ai/settings.py
@@ -87,6 +87,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.request',
     'django.core.context_processors.static',
+    'notification.context_processors.notification',
 )
 
 MIDDLEWARE_CLASSES = (
