c3d0bbd90a86423f50e5ece207ec247078378c31
Jack LaBarba <brakit@gmail.com>
2013-07-30 16:01:59 -0700

    Finalized new urgent web corrections notification

--- articleflow/management/commands/test_notification.py
+++ articleflow/management/commands/test_notification.py
@@ -1,11 +1,13 @@
 from django.core.management.base import BaseCommand, CommandError
 import notification.models as n
+from articleflow.models import Article
 
 from django.contrib.auth.models import User
 
 def main(*args, **options):
     u = User.objects.get(username='jlabarba')
-    n.send([u], 'hello_world')
+    a = Article.objects.get(doi='pone.0012345')
+    n.send_now([u], 'new_urgent_web_correction', {"article": a})
     
 class Command(BaseCommand):
     def handle(self, *args, **options):
--- articleflow/notification_setup.py
+++ articleflow/notification_setup.py
@@ -4,9 +4,9 @@ def create_notification_types(verbosity=2):
     print "Initializing notifications ..."
 
     notification.NoticeType.create(\
-        label="new_urgent_qc",
-        display="New Urgent QC",
-        description="New article needing urgent QC",
+        label="new_urgent_web_correction",
+        display="New Urgent Web Corrections",
+        description="New article needing urgent web corrections",
         default=2,
         verbosity=verbosity)
 
