4e906a1db4a837be409e9a5d32f6b93e757ae1aa
Jack LaBarba <brakit@gmail.com>
2013-08-13 15:12:39 -0700

    Adds tests for urgent wc automated transition

--- articleflow/tests/transitions.py
+++ articleflow/tests/transitions.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta, date
+
 from articleflow.daemons.transition_tasks import *
 from django.test import TestCase
 
@@ -16,7 +18,10 @@ class TransitionTasksTestCaseNoAmbra(TestCase):
 
         self.s_ingested = State.objects.get(unique_name='ingested')
         self.s_wc_cw = State.objects.get(unique_name='needs_web_corrections_cw')
-        self.s_wc_merops = State.objects.get(unique_name='needs_web_corrections_merops')
+        self.s_wc_merops = State.objects.get(unique_name='needs_web_corrections_merops')        
+        self.s_urg_wc_cw = State.objects.get(unique_name='urgent_web_corrections_cw')
+        self.s_urg_wc_merops = State.objects.get(unique_name='urgent_web_corrections_merops')
+
         self.s_qc_cw = State.objects.get(unique_name='ready_for_qc_cw')
         self.s_qc_merops = State.objects.get(unique_name='ready_for_qc_merops')
 
@@ -51,3 +56,39 @@ class TransitionTasksTestCaseNoAmbra(TestCase):
         self.art_1 = Article.objects.get(doi='pone.0000001')
         self.assertEqual(self.art_1.current_state, self.s_qc_cw)
 
+    def test_urgent_web_corrections(self):
+        # CW
+        ArticleState(article=self.art_3, state=self.s_wc_cw).save()
+        pubdate = add_workdays(datetime.utcnow(), 3)
+        self.art_3.pubdate = pubdate
+        self.art_3.save()
+
+        assign_urgent_corrections(2)
+        self.art_3 = Article.objects.get(doi='pone.0000003')
+        self.assertEqual(self.art_3.current_state, self.s_wc_cw)
+
+        pubdate = add_workdays(datetime.utcnow(), 1)
+        self.art_3.pubdate = pubdate
+        self.art_3.save()
+
+        assign_urgent_corrections(2)
+        self.art_3 = Article.objects.get(doi='pone.0000003')
+        self.assertEqual(self.art_3.current_state, self.s_urg_wc_cw)
+    
+        # Merops
+        ArticleState(article=self.art_1, state=self.s_wc_merops).save()
+        pubdate = add_workdays(datetime.utcnow(), 3)
+        self.art_1.pubdate = pubdate
+        self.art_1.save()
+
+        assign_urgent_corrections(2)
+        self.art_1 = Article.objects.get(doi='pone.0000001')
+        self.assertEqual(self.art_1.current_state, self.s_wc_merops)
+
+        pubdate = add_workdays(datetime.utcnow(), 1)
+        self.art_1.pubdate = pubdate
+        self.art_1.save()
+
+        assign_urgent_corrections(2)
+        self.art_1 = Article.objects.get(doi='pone.0000001')
+        self.assertEqual(self.art_1.current_state, self.s_urg_wc_merops)
