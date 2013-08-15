eca784bde74ffaa1664fcb8316bf3e7d1b805989
Jack LaBarba <brakit@gmail.com>
2013-08-15 10:09:46 -0700

    Changes name of merops counts report to qa automation performance

--- articleflow/reports_urls.py
+++ articleflow/reports_urls.py
@@ -4,5 +4,5 @@ from articleflow.views import *
 urlpatterns = patterns('articleflow.views',
                        url(r'^$', ReportsMain.as_view(), name='reports_main'),
                        url(r'^pc_qc_counts/$', ReportsPCQCCounts.as_view(), name='reports_pc_qc_counts'),
-                       url(r'^merops_counts/$', ReportMeropsCounts.as_view(), name='reports_merops_counts'),
+                       url(r'^qa_automation_performance/$', ReportMeropsCounts.as_view(), name='reports_merops_counts'),
                        )
