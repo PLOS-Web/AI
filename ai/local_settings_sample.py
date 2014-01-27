d59d0ccd1a8fe949dc084bf5cf826ee937ac4713
Jack LaBarba <brakit@gmail.com>
2014-01-27 09:54:42 -0800

    Updates local settings template

--- ai/local_settings_sample.py
+++ ai/local_settings_sample.py
@@ -1,4 +1,6 @@
 # Fill me out and copy me to local_settings.py
+#  Things that are necessary for the operation of the base web 
+#  server are marked, **REQUIRED**.
 #  (then don't ever commit me)
 
 import os
@@ -6,25 +8,41 @@ import ldap
 import sys
 from django_auth_ldap.config import LDAPSearch
 
+"""
+ DEBUG=True will show useful 500 traces and stuff
+"""
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-LOG_FILE_DIRECTORY = os.path.dirname("/home/jlabarba/ai/logs/")
+"""
+ **REQUIRED**
+   Be sure to create this directory first
+"""
+LOG_FILE_DIRECTORY = os.path.dirname("/home/jlabarba/ai-logs/")
 
-# email to send 500 stack traces to
+"""
+ Admin Email Accounts
+  Needs smtp info to work (at bottom
+  email to send 500 stack traces to
+  DOESN'T affect any user accounts
+"""
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
+"""
+ Backend Database
+  Leave alone to use a crummy sqlite backend.
+  HOWEVER, if you plan on doing a full load of production data
+  you'll need to create and use a mysql db instead.
+  for more info, see 
+    https://docs.djangoproject.com/en/dev/intro/tutorial01/#database-setup
+"""
 DATABASES = {
     # AI backend
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'crummy_db.sqlite',
     }
 }
 
@@ -34,8 +52,11 @@ if 'test' in sys.argv:
         'NAME': 'test_db'
         }
     SOUTH_TESTS_MIGRATE = False
-    
-# Settings for the EM reporting datasource
+
+"""    
+ only required for scheduler tasks
+  Settings for the EM reporting datasource
+"""
 EM_REPORTING_DATABASE = {
     'USER': '',
     'PASSWORD': '',
@@ -43,6 +64,9 @@ EM_REPORTING_DATABASE = {
     'PORT': '',
 }
 
+"""
+ only required for scheduler tasks
+"""
 AMBRA_STAGE_DATABASE = {
     'USER': '',
     'PASSWORD': '',
@@ -51,6 +75,9 @@ AMBRA_STAGE_DATABASE = {
     'NAME': '',
 }
 
+"""
+ only required for scheduler tasks
+"""
 AMBRA_PROD_DATABASE = {
     'USER': '',
     'PASSWORD': '',
@@ -59,18 +86,27 @@ AMBRA_PROD_DATABASE = {
     'NAME': '',
 }
 
-# generate one here 
-# http://www.miniwebtool.com/django-secret-key-generator/
+"""
+ generate one here and paste in
+   http://www.miniwebtool.com/django-secret-key-generator/
+"""
 SECRET_KEY = ''
 
-# LDAP config
+"""
+ LDAP config
+  Requried if you want login authentication to understand plos
+  usernames and passwords
+"""
 AUTH_LDAP_SERVER_URI = ''
 AUTH_LDAP_BIND_DN = ''
 AUTH_LDAP_BIND_PASSWORD = ''
 AUTH_LDAP_USER_SEARCH = LDAPSearch("OU=PLoS,DC=plos,DC=org",
     ldap.SCOPE_SUBTREE, "(sAMAccountName=%(user)s)")
 
-# debug dispatch email config
+"""
+ SMTP
+   required if you want to test email notifications 
+"""
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
